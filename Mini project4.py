e498657ec65964d3b18953d419958de1fec8a78c
Gaya3666 <89004979+Gaya3666@users.noreply.github.com>
2021-08-27 23:18:16 +0530

    Create Mini project4.py

---
+++ Mini project4.py
@@ -0,0 +1,7 @@
+echo "# mini-project-3" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Gaya3666/mini-project-3.git
+git push -u origin main
